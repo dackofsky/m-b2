fc6a9980986a5415a69d390e2404b5b4e27e0f7b
EIJI MORI <dackofsky77@yahoo.co.jp>
2015-08-31 09:14:10 +0000

    add edit*destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,48 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
     #Messageを全て取得
     @messages = Message.all
+    @message= Message.new
   end
   
   
   def create
     @message = Message.new(message_params)
-    @message.save
+    if @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
+  else
+    # メッセージが保存できなかった時
+    @messages = Message.all
+    flash.now[:alert] = "メッセージの保存に失敗しました。"
+    render 'index'
+    end
+  end  
+  
+  def edit
   end
+  
+  def update
+  if @message.update(message_params)
+      #保存に成功した場合は編集画面へ戻す
+      render 'edit'
+  end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: "メッセージを削除しました"
+  end
+  
+  
   
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ##ここまで
+ 
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
+
